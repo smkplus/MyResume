e820a1e4b9c41c71aedfb313e6b283bf25fae3f8
Seyed Morteza Kamali <smkplus3d@gmail.com>
2022-08-06 13:04:57 +0430

    fix minor changes

--- js/init.js
+++ js/init.js
@@ -102,20 +102,7 @@
 /*	Modal Popup
 ------------------------------------------------------*/
 
-    $('.item-wrap a').magnificPopup({
 
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
-
-    });
-
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
 
 
 /*----------------------------------------------------*/
